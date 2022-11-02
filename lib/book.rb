dadf3c9fe8116fa7879c673de1953f57cb355e65
Brandon Moreno <brandonm99@me.com>
2022-11-02 10:47:38 -0600

    commit tree 0f7c476e46516c9e6ee3c337a2e162767c8ffab7

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+require 'pry'
 
+class Book
+  attr_accessor :title, :author, :page_count, :genre
 
+  def initialize(title)
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end   
+end
+
+binding.pry
